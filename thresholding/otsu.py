edfe6d04656d0bb12533d861b684b4b47426a0de
Najib Ishaq <najib_ishaq@zoho.com>
2022-06-30 15:11:18 -0400

    wip: minor tweaks

--- thresholding/otsu.py
+++ thresholding/otsu.py
@@ -1,15 +1,9 @@
 import numpy
 
 
-BIN_COUNT = 64
-
-
-def find_threshold(values: numpy.ndarray) -> float:
-    # Set total number of bins in the histogram
-    bins_num = BIN_COUNT
-
+def find_threshold(values: numpy.ndarray, num_bins: int = 256) -> float:
     # Get the image histogram
-    hist, bin_edges = numpy.histogram(values, bins=bins_num)
+    hist, bin_edges = numpy.histogram(values, bins=num_bins)
 
     # Get normalized histogram if it is required
     # if is_normalized:
@@ -29,8 +23,8 @@ def find_threshold(values: numpy.ndarray) -> float:
 
     inter_class_variance = weight1[:-1] * weight2[1:] * (mean1[:-1] - mean2[1:]) ** 2
 
-    # Maximize the inter_class_variance function val
-    index_of_max_val = numpy.argmax(inter_class_variance)
+    # Maximize the inter_class_variance
+    index_of_max_var = numpy.argmax(inter_class_variance)
 
-    threshold = bin_mids[:-1][index_of_max_val]
+    threshold = bin_mids[:-1][index_of_max_var]
     return threshold
