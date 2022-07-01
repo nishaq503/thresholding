a7069666003636a9b26822c70b23ff8aeb5f1f1c
Najib Ishaq <najib_ishaq@zoho.com>
2022-07-01 10:47:23 -0400

    wip: added fpr threshold

---
+++ thresholding/custom_fpr.py
@@ -0,0 +1,30 @@
+import statistics
+
+import numpy
+
+
+def find_threshold(
+        values: numpy.ndarray,
+        false_positive_rate: float,
+) -> float:
+    """ Computes a threshold value using a user-specified false positive rate.
+
+    We assume that the `negative_values` follow a single gaussian distribution.
+     We estimate the mean and standard deviation of this distribution and
+     compute a threshold such that the area to the right of the threshold is
+     equal to the given `false_positive_rate`.
+
+    Args:
+        values: drawn from a single gaussian distribution.
+        false_positive_rate: A user-defined tuning parameter.
+
+    Returns:
+        The computed threshold value.
+    """
+    mu = float(numpy.mean(values))
+    sigma = float(numpy.mean(values))
+
+    distribution = statistics.NormalDist(mu, sigma)
+    threshold = distribution.inv_cdf(1 - false_positive_rate)
+
+    return threshold
