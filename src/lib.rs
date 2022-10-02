811b79ce7d90c6f7b085e081702d39e55ef5843d
Najib Ishaq <najib_ishaq@zoho.com>
2022-10-02 16:50:43 -0400

    minor updates

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,11 @@ mod utils;
 use numpy::PyReadonlyArray1;
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
 fn n_sigma_threshold(values: PyReadonlyArray1<f64>, n: f64) -> PyResult<f64> {
     Ok(n_sigma::find_threshold(values.as_array().as_slice().unwrap(), n))
 }
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn thresholding(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(n_sigma_threshold, m)?)?;
