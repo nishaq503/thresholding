f74e4fff20fe49f0f92821e23dc2968d8073a90f
Najib Ishaq <najib_ishaq@zoho.com>
2022-10-02 16:50:10 -0400

    added python docs

--- thresholding.pyi
+++ thresholding.pyi
@@ -1,1 +1,14 @@
-def n_sigma_threshold(a: int, b: int) -> str: ...
+import numpy
+
+
+def n_sigma_threshold(values: numpy.ndarray, n: float) -> float:
+    """ Computes the threshold as `mean + n * standard_deviation`.
+
+    Args:
+        values: over which to compute the threshold.
+        n: number of standard deviations to go away from the mean.
+
+    Returns:
+        The n-sigma threshold
+    """
+    ...
